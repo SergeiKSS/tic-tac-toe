d151ed45e74539f867268510f23b49a4d6a68b10
SergeiKSS <SergeiKSS@mail.ru>
2017-02-04 18:03:34 +0200

    tic-tac-toe:return correct player symbol

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,31 @@
 class TicTacToe {
     constructor() {
-
+        var step, field = [[],[],[]];
+        this.step = 'x';
+        this.field = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+            ];
     }
 
     getCurrentPlayerSymbol() {
-
+        if (this.step == 'x'){
+            return 'x';}
+        else{
+            return 'o';
+        }
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if (this.field[rowIndex][columnIndex] == 0){ 
+            this.field[rowIndex][columnIndex] = this.step;
+            if (this.step == 'x'){
+                this.step = 'o';
+            }else{
+                this.step = 'x';
+            }
+        }
     }
 
     isFinished() {
